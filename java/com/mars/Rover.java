fd09097660e3f79169020c2476bc37e2b9144b4b
Matthew Baker <mu.beta.06@gmail.com>
2013-02-19 10:52:14 +1100

    added stub for java implementation.

---
+++ java/com/mars/Rover.java
@@ -0,0 +1,13 @@
+package com.mars;
+
+public class Rover {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ java/com/mars/RoverController.java
@@ -0,0 +1,13 @@
+package com.mars;
+
+public abstract class RoverController {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
